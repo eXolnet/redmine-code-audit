3636f083d279f12b93298c454ed20c5aa525f488
www-data <www-data@dev.exolnet.com>
2013-05-29 16:33:07 -0400

    Ajout de la migration

---
+++ db/migrate/001_create_audits.rb
@@ -0,0 +1,28 @@
+class CreateAudits < ActiveRecord::Migration
+  def change
+    create_table :audits do |t|
+      t.integer  :user_id,           :null => false
+      t.integer  :changeset_id,      :null => false
+      t.string   :summary,           :null => false
+      t.text     :details
+      t.datetime :created_on,        :null => false
+      t.datetime :updated_on,        :null => false
+    end
+    
+    create_table :audit_comments do |t|
+      t.integer  :audit_id,          :null => false
+      t.integer  :user_id,           :null => false
+      t.text     :content
+      t.datetime :created_on,        :null => false
+      t.datetime :updated_on,        :null => false
+    end
+    
+    create_table :audit_comment_inlines do |t|
+      t.integer  :audit_comment_id,  :null => false
+      t.integer  :change_id,         :null => false
+      t.integer  :line_begin,        :null => false
+      t.integer  :line_end
+      t.text     :content
+    end
+  end
+end
