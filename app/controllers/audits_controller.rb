a2465640dc83574ba7681bef9fc06010bc31115f
www-data <www-data@dev.exolnet.com>
2013-05-31 10:11:06 -0400

    Correction du système de pagination pour redmine 2.2.x

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -11,7 +11,7 @@ class AuditsController < ApplicationController
   	@limit = per_page_option
   	@audit_count = @query.count
     @audit_pages = Paginator.new self, @audit_count, @limit, params['page']
-    @offset ||= @audit_pages.offset
+    @offset ||= @audit_pages.current.offset
   	
   	@audits = @query
   		.order("#{Audit.table_name}.updated_on DESC")
